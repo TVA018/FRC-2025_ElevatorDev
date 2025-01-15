5ff1434f4c543f4ca8be63a233fd5b875d0a4f5c
TVA018 <161531333+TVA018@users.noreply.github.com>
2025-01-15 11:47:50 -0500

    Update ElevatorIO.java

--- src/main/java/ca/warp7/frc2025/subsystems/elevator/ElevatorIO.java
+++ src/main/java/ca/warp7/frc2025/subsystems/elevator/ElevatorIO.java
@@ -1,6 +1,7 @@
 package ca.warp7.frc2025.subsystems.elevator;
 
 public interface ElevatorIO {
+    public abstract void setVoltage(double volts);
     public abstract void zeroMotor();
     public abstract void limitSwitch();
 }
