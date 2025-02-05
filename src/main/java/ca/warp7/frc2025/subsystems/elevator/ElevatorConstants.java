348072242d65ad8a75d6d7c8774b4e31d26e3476
TVA018 <thomas.vu018@gmail.com>
2025-02-04 20:59:52 -0500

    Update ElevatorConstants.java

--- src/main/java/ca/warp7/frc2025/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/ca/warp7/frc2025/subsystems/elevator/ElevatorConstants.java
@@ -7,6 +7,23 @@ public class ElevatorConstants {
     public static final double GEAR_RATIO = 12.5 / 1.0; // Temporary value, not accurate
     public static final double DRUM_RADIUS_METERS = Units.inchesToMeters(5); // Temporary value, not accurate
     public static final Rotation2d ELEVATOR_ANGLE = Rotation2d.fromDegrees(80.0);
+    
+    public static enum LEVEL {
+        L1 (0), 
+        L2 (5), 
+        L3 (10), 
+        L4 (15);
+
+        private final double position;
+
+        private LEVEL(double position){
+            this.position = position;
+        }
+
+        public double getPosition(){
+            return position;
+        }
+    }
 
     public static final record PIDGains(double kP, double kI, double kD) {}
 
